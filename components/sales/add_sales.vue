f411d2795f580f407d5d116f8d7f1631bfaf5ad2
Kharyl Kate <kharyl.kate.ortiz@biotechfarms.net>
2020-09-17 17:21:54 -0700

    m

--- components/sales/add_sales.vue
+++ components/sales/add_sales.vue
@@ -11,19 +11,34 @@
         <div class="modal-body">
           <form action="" id="add_item_form">
             <div class="form-row">
-                <div class="form-group col-md-3">
-                  <label for="input_rtransaction_date">Date: </label>
-                  <input type="date" v-model="st.stransaction_date" class="form-control form__date" id="input_rtransaction_date" default="06/06/2020" disabled>
-                </div>
-                <div class="form-group col-md-3">
-                  <label for="input_stransaction_no">Sales Official Receipt Number: </label>
-                  <input type="number" v-model="st.or_no" class="form-control form__orNo" placeholder="1654325" id="input_stransaction_no" disabled>
-                </div>
-
+              <div class="form-group col-md-3">
+                <label for="input_rtransaction_date">Date: </label>
+                <input type="date" v-model="st.stransaction_date" class="form-control form__date" id="input_rtransaction_date" value="06/06/2020" >
               </div>
-
+              <div class="form-group col-md-3">
+                <label for="input_stransaction_no">Sales Official Receipt Number: </label>
+                <input type="number" v-model="st.or_no" class="form-control form__orNo" value="1654325" id="input_stransaction_no">
+              </div>
+            </div>
+<hr>
+            <div class="form-row mt-4">
+              <div class="col-md-12"><p>Customer Information: </p></div>
+              <div class="form-group col-md-3">
+                <label for="input_cus_name">Customer Name: </label>
+                <input type="text" v-model="st.customer_name" class="form-control form__cusname" id="input_cus_name" placeholder="Name">
+              </div>
+              <div class="form-group col-md-3">
+                <label for="input_contno">Contact Number: </label>
+                <input type="number" v-model="st.customer_contact_no" class="form-control cont_no" placeholder="Contact Number" id="input_contno">
+              </div>
+              <div class="form-group col-md-6">
+                <label for="cus_address">Address: </label>
+                <input type="text" v-model="st.customer_address" class="form-control cus_address" placeholder="Address" id="cus_address">
+              </div>
+            </div>
+<hr>
               <div class="top-name d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 mt-3 px-2" id="topName">
-                <!-- {{salesList}} -->
+                
                     <label for="">Sales Transaction Items:</label>
                     <div class="btn-toolbar mb-2 mb-md-0">
                     <button type="button" class="btn btn-sm lg-btn btn_rtrans text-white" id="btn_rtransaction_add" @click="addRow">
@@ -43,7 +58,7 @@
                           <label for="form__description">Product Description:</label>
                         </div>
                         <div class="form-group col-md-2">
-                          <label for="form__qty">Quantity:</label>
+                          <label for="form__quantity">Quantity:</label>
                         </div>
                         <div class="form-group col-md-3">
                           <label for="form__unitcost">Cost Per Unit:</label>
@@ -54,21 +69,25 @@
                       </div>
                   </li>
                   <li v-for="(row, index) in rows" :key="index.id">
-                  <div class="form-row d-flex col-md-12 mt-0">
+
+                  <div class="form-row d-flex col-md-12 mt-0 mb-0">
                     <div class="form-group col-md-3">
-                      <input type="text" v-model="row.barcode" class="form-control form__barcode" placeholder="barcode" id="rtransaction_barcode">
+                      <input @keyup.enter="getbarcode()" type="text" list="barcode_list" v-model="row.barcode" class="form-control form__barcode" placeholder="barcode" id="rtransaction_barcode" autocomplete="off">
+                        <datalist id="barcode_list">
+                          <option v-for="items in inventoryList" :key="items.id" :value="items.barcode">{{items.product_description}}</option>
+                        </datalist>
                     </div>
                     <div class="form-group col-md-3">
-                      <input type="text" v-model="row.description" class="form-control form__description" placeholder="Product Description">
+                      <input type="text" v-model="row.product_description" class="form-control form__description" placeholder="Product Description" disabled>
                     </div>
                     <div class="form-group col-md-2">
-                      <input type="text" v-model="row.qty" class="form-control form__qty" placeholder="Quantity">
+                      <input type="text" v-model="row.quantity" class="form-control form__quantity" placeholder="Quantity">
                     </div>
                     <div class="form-group col-md-3">
-                      <input type="text" v-model="row.unitcost" class="form-control form__unitcost" placeholder="Cost Per Unit">
+                      <input type="text" v-model="row.unit_cost" class="form-control form__unitcost" placeholder="Cost Per Unit" disabled>
                     </div>
                     <div class='form-group col-md-1'>
-                      <button class="btn btn-danger rem_item" type="button" @click="removeElement" id="Action">Remove</button>
+                      <button class="btn btn-danger rem_item text-white" type="button" @click="removeElement" id="Action">Remove</button>
                     </div>
                   </div>
                 </li>
@@ -87,9 +106,10 @@
                 </div>
               </div>
 
+
             <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" id="salescancel" data-dismiss="modal">Cancel</button>
-              <button type="button" @click="saveSales()" class="btn btn-primary">OK</button>
+              <button type="button" class="btn btn-secondary text-white" id="salescancel" data-dismiss="modal">Cancel</button>
+              <button type="button" @click="saveSales()" class="btn btn-primary text-white">OK</button>
             </div>
             <!-- <button class="btn btn-primary" type="submit">Submit form</button> -->
           </form>
@@ -113,15 +133,13 @@ export default {
       }),
     },
     data() {
-      // return {
-      //   samp: 0
-      // },
       return {
         st: {},
+        inv: {},
         rows: [{
           barcode: "",
-          description: "",
-          qty: "",
+          product_description: "",
+          quantity: "",
           unitcost: ""
         }]
       }
@@ -131,21 +149,53 @@ export default {
         var elem = document.createElement('li');
         this.rows.push({
           barcode: "",
-          description: "",
-          qty: "",
+          product_description: "",
+          quantity: "",
           unitcost: ""
         });
       },
       removeElement: function(index){
-        if((this.rows.length-1)!=0){
-          this.rows.splice(index,1)
+        this.rows.splice(index,1)
+      },
+      getbarcode(){
+        console.log('barcode?', this.rows[this.rows.length-1].barcode)
+        console.log(this.inventoryList);
+
+        for(var i = 0; i < this.inventoryList.length; i++){
+          if(this.inventoryList[i].barcode == this.rows[this.rows.length-1].barcode){
+            this.rows[this.rows.length-1].product_description = this.inventoryList[i].product_description
+            this.rows[this.rows.length-1].unit_cost = this.inventoryList[i].unit_cost
+          }
         }
-      }
+        
+        console.log('rows: ',this.rows)
+        // this.rows[this.rows.length-1].product_description = bc.product_description
+        // console.log('product info: ', this.rows[this.rows.length-1].barcode, ', ', this.rows[this.rows.length-1].product_description);
+        
+
+      },
+      ...mapActions(['addSales', 'updateInvQty']),
+      saveSales(){
+        this.rows.or_no = this.st.or_no
+        this.rows.transaction = 'sales'
+        this.st.items = this.rows 
+        this.addSales({
+          sales: this.st
+        })
+        
+        
+        this.updateInvQty({
+          invqty: this.rows
+        })
+
+        $("#addSales").modal('hide');
+        $("#add_item_form")[0].reset();
+      },
     },
     created(){
-      // console.log('hilu',this.inventoryList)
+      //console.log('hilu',this.inventoryList)
     },
-    async breforeCreate(){
+    async beforeCreate(){
       await this.$store.dispatch("fetchSalesList")
       await this.$store.dispatch("fetchInventoryList")
     }
@@ -154,9 +204,3 @@ export default {
 
 
 </script>
-
-<style scoped>
-.li {
-  margin: 0px
-}
-</style>
